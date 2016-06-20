ec1e95fc64ec7038acd22bcb0279186f76b4cb62
Rameen Mahdavi <rlmahdavi@gmail.com>
2016-06-20 11:14:22 -0700

    Fixed move bug

--- TKGridTest.py
+++ TKGridTest.py
@@ -1,6 +1,7 @@
 from Tkinter import *
 import time
 import random
+from collections import deque
 
 #####################
 ##### CLASSES #######
@@ -38,15 +39,18 @@ class Tile:
 class Grid:
 	def __init__(self):
 		self.playGrid = []
-
+		self.testGrid = [[0 for i in range(gridSize)] for j in range(gridSize)]
+		self.testGrid[1][5] = "hello"
+		print self.testGrid
 		#create tiles
-		for i in range(gridSize * gridSize):
+		for i in range(lastTile):
 			#i * tileSize gets you to the left side, adding half of tilesize gets you to the center, modulus keeps it in rows
-			centerX = (i * tileSize + (tileSize / 2)) % (maxSize)
+			centerX = (i * tileSize + (tileSize / 2)) % (lastPixel)
 			#i / grisize gets you the row #, * tilsize gets you the bottom of the rectangle, - tilesize/2 gets you the center
 			centerY = ((i / gridSize) + 1) * tileSize - (tileSize / 2)
 			#RRGGBB more green as it goes to the right, more blue as it goes down
-			color = "#00" + "%02x" % ((i % gridSize) * (256 / gridSize)) + "%02x" % ((i / gridSize) * (256 / gridSize))
+			#color = "#00" + "%02x" % ((i % gridSize) * (256 / gridSize)) + "%02x" % ((i / gridSize) * (256 / gridSize))
+			color = "#909090"
 			#color = random.choice(colors)
 			self.playGrid.append(Tile(tileSize, centerX, centerY, i, color))
 
@@ -63,7 +67,6 @@ class Grid:
 			#canvas.create_text(tile.centerX, tile.centerY, text=tile.tileNum)
 			canvas.pack()
 
-
 	#returns number of clicked tile, based off x & y coords
 	def findTile(self, xpos, ypos):
 		gridX = xpos / tileSize
@@ -73,8 +76,8 @@ class Grid:
 
 class POI:
 	def __init__(self, xpos, ypos, tag, color, symbol, loc):		
-		self.gridX = xpos
-		self.gridY = ypos
+		#self.gridX = xpos
+		#self.gridY = ypos
 		self.tag = tag
 		self.color = color
 		self.symbol = symbol
@@ -90,6 +93,12 @@ class POI:
 				 tag=self.tag)
 		canvas.create_text(grid.playGrid[self.loc].centerX, grid.playGrid[self.loc].centerY, text=self.symbol, tag=self.tag)
 
+class Pathfinder:
+	def __init__(self):
+		frontier = deque()
+	#frontier.append()
+	#frontier.popleft()
+
 #####################
 ##### FUNCTIONS #####
 #####################
@@ -104,8 +113,9 @@ def drawEntities():
 
 colors = ["white", "red", "green", "blue", "cyan", "yellow", "magenta"]
 gridSize = 16
+lastTile = gridSize * gridSize
 tileSize = 32
-maxSize = gridSize * tileSize
+lastPixel = gridSize * tileSize
 
 #####################
 ####### MAIN ########
@@ -118,6 +128,7 @@ frame = Frame(root)
 frame.pack()
 frame.focus_set()
 
+prevClickedEntity = None
 def dispTileInfo(event):
 	global prevClickedEntity
 	frame.focus_set()
@@ -130,6 +141,7 @@ def dispTileInfo(event):
 
 	#if you're selecting an entity
 	if clickedEntity is not None and prevClickedEntity is None:
+		clickedEntity.loc = None
 		prevClickedEntity = clickedEntity
 		canvas.delete(clickedEntity.tag)
 	#if you've selected an entity and are clicking an empty tile
@@ -140,11 +152,16 @@ def dispTileInfo(event):
 	#otherwise toggle passable
 	else:
 		clickedTile.togglePassable()
-prevClickedEntity = None
 
-canvas = Canvas(root, width=maxSize, height=maxSize)
+canvas = Canvas(root, width=lastPixel, height=lastPixel)
 canvas.bind("<Button-1>", dispTileInfo)
 canvas.pack()
+
+def findPathButton():
+	print "gettin there"
+
+stepButton = Button(root, text=">", command=findPathButton)
+stepButton.pack()
 
 grid = Grid()
 grid.update() #seedEntry.get() if going back to random colors
@@ -153,7 +170,7 @@ entities = []
 player = POI(xpos=3, ypos=2, tag="player", color="yellow", symbol="@", loc=0)
 entities.append(player)
 
-exit = POI(xpos=gridSize, ypos=gridSize, tag="exit", color="red", symbol="E", loc=gridSize * gridSize - 1)
+exit = POI(xpos=gridSize, ypos=gridSize, tag="exit", color="red", symbol="E", loc=lastTile - 1)
 entities.append(exit)
 
 drawEntities()
