fd9b1ac3597a36dded2b3f2ae0d6f57bcf0f3eb3
Rameen Mahdavi <rlmahdavi@gmail.com>
2016-06-23 13:12:14 -0700

    Step Count

--- TKGridTest.py
+++ TKGridTest.py
@@ -30,20 +30,21 @@ class Tile:
 		if self.passable == False:
 			color = "black"
 		elif self.frontier == True:
-			color = "#404090"
+			color = "#404090" #blue
 		elif self.visited == True:
-			color = "#409040"
+			color = "#409040" #green
 		else:
-			color = "#808080"
+			color = "#808080" #gray
 
 		canvas.create_rectangle(self.centerX - tileSize / 2,
 						   self.centerY - tileSize / 2,
 						   self.centerX + tileSize / 2, 
 						   self.centerY + tileSize / 2,
 						   fill=color)
+
 	def togglePassable(self):
 		self.passable = not self.passable
-		drawAll()
+		self.draw()
 
 #a gridSize x gridSize array of Tiles
 class Grid:
@@ -112,15 +113,17 @@ class POI:
 class Pathfinder:
 	def __init__(self):
 		self.frontier = deque()
-		self.reset()
+		self.stepCount = 0
 
 	def expandFrontier(self):
 		if len(self.frontier) == 0:
 			return
+		self.stepCount += 1
+		stpct.set("Step Count: " + str(self.stepCount))
 		workingTile = self.frontier.popleft()
 		workingTile.visited = True
 		workingTile.frontier = False
-		print "\n\nworkingTile", workingTile.row, workingTile.col
+		workingTile.draw()
 
 		possibleFrontier = ((workingTile.row+1, workingTile.col),
 							(workingTile.row-1, workingTile.col),
@@ -128,15 +131,15 @@ class Pathfinder:
 							(workingTile.row, workingTile.col-1))
 
 		for loc in possibleFrontier:
-			drawAll()
-			tile = grid.playGrid[loc[0]][loc[1]]
-			if tile.visited == False and tile not in self.frontier and inRange(loc[0], loc[1]) and tile.passable == True:
-				self.frontier.append(tile)
-				print "appended tile at", tile.row, tile.col
-				tile.frontier = True
+			if inRange(loc[0], loc[1]):
+				tile = grid.playGrid[loc[0]][loc[1]]
+				if tile.visited == False and tile not in self.frontier and tile.passable == True:
+					self.frontier.append(tile)
+					tile.frontier = True
+					tile.draw()
+					drawEntities()
 
-		for tile in self.frontier:
-			print tile.row, tile.col
+
 
 	def clear(self):
 		self.frontier.clear()
@@ -150,9 +153,7 @@ class Pathfinder:
 
 	def reset(self):
 		self.frontier.append(grid.playGrid[player.row][player.col])
-
-	#frontier.append()
-	#frontier.popleft()
+		self.stepCount = 0
 
 #####################
 ##### FUNCTIONS #####
@@ -171,6 +172,38 @@ def inRange(row, col):
 		return True
 	else:
 		return False
+
+#for when you click on a tile
+def clickTile(event):
+	global prevClickedEntity #no statics :(
+	frame.focus_set()
+	clickedTile = grid.findTile(event.x, event.y)
+	clickedEntity = None
+
+	#find if there's an entity on the clicked tile
+	for entity in entities:
+		if entity.row == clickedTile.row and entity.col == clickedTile.col:
+			clickedEntity = entity
+
+	#if you're selecting an entity for the first time, remove it from board
+	if clickedEntity is not None and prevClickedEntity is None:
+		clickedEntity.row = None
+		clickedEntity.col = None
+		prevClickedEntity = clickedEntity
+		canvas.delete(clickedEntity.tag)
+		pathfinder.clear()
+	#if you've previously selected an entity and are clicking an empty tile, add the prev entity to the board
+	elif clickedEntity is None and prevClickedEntity is not None:
+		prevClickedEntity.row = clickedTile.row
+		prevClickedEntity.col = clickedTile.col
+		drawEntities()
+		if prevClickedEntity == player:
+			pathfinder.reset()
+		prevClickedEntity = None
+	#otherwise toggle passable
+	else:
+		clickedTile.togglePassable()
+
 
 #####################
 ##### CONSTANTS #####
@@ -192,50 +225,30 @@ lastVPixel = numCols * tileSize
 root = Tk()
 root.wm_title("Pathfinder")
 
+pathfinder = Pathfinder()
+
 frame = Frame(root)
-frame.pack()
-frame.focus_set()
+frame.pack(side=LEFT)
 
 prevClickedEntity = None
-def dispTileInfo(event):
-	global prevClickedEntity
-	frame.focus_set()
-	clickedTile = grid.findTile(event.x, event.y)
-	print "clicked", clickedTile.row, clickedTile.col
-	clickedEntity = None
-	#find if there's an entity on the clicked tile
-	for entity in entities:
-		if entity.row == clickedTile.row and entity.col == clickedTile.col:
-			clickedEntity = entity
 
-	#if you're selecting an entity
-	if clickedEntity is not None and prevClickedEntity is None:
-		clickedEntity.row = None
-		clickedEntity.col = None
-		prevClickedEntity = clickedEntity
-		canvas.delete(clickedEntity.tag)
-		pathfinder.clear()
-	#if you've selected an entity and are clicking an empty tile
-	elif clickedEntity is None and prevClickedEntity is not None:
-		prevClickedEntity.row = clickedTile.row
-		prevClickedEntity.col = clickedTile.col
-		drawEntities()
-		if prevClickedEntity == player:
-			pathfinder.reset()
-		prevClickedEntity = None
-	#otherwise toggle passable
-	else:
-		clickedTile.togglePassable()
-
-canvas = Canvas(root, width=numCols * tileSize, height=numRows * tileSize)
-canvas.bind("<Button-1>", dispTileInfo)
+canvas = Canvas(frame, width=numCols * tileSize, height=numRows * tileSize)
+canvas.bind("<Button-1>", clickTile)
 canvas.pack()
+
+optionsPanel = Frame(root)
+optionsPanel.pack(side=RIGHT)
+
+stpct = StringVar()
+steps = Label(optionsPanel, width=14, textvariable=stpct)
+steps.pack(side=LEFT)
+stpct.set("Step Count: 0")
 
 def findPathButton():
 	pathfinder.expandFrontier()
 
-stepButton = Button(root, text=">", command=findPathButton)
-stepButton.pack()
+stepButton = Button(optionsPanel, text=">", command=findPathButton)
+stepButton.pack(side=RIGHT)
 
 grid = Grid()
 grid.drawTiles() #seedEntry.get() if going back to random colors
@@ -250,7 +263,7 @@ exit.placeAt(numRows-1, numCols-1)
 entities.append(exit)
 
 
-pathfinder = Pathfinder()
+pathfinder.reset()
 drawEntities()
 #main loop
 root.mainloop()
